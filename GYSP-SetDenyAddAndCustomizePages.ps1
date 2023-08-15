91c83758c79cace914b92d0e3facabc6e21dfc01
Great-Yarmouth-Space-Program <141946788+Great-Yarmouth-Space-Program@users.noreply.github.com>
2023-08-15 11:10:23 +0100

    Update GYSP-SetDenyAddAndCustomizePages.ps1

--- GYSP-SetDenyAddAndCustomizePages.ps1
+++ GYSP-SetDenyAddAndCustomizePages.ps1
@@ -1,4 +1,17 @@
+﻿<#
+    Name:               GYSP-SetDenyAddAndCustomizePages.ps1
+    Version:            1.0
+    Date:               11-08-2023
+    Original Author:    Si Ansell
+    Email:              graph@greatyarmouthspaceprogram.space
 
+    Modules:            ExchangeOnlineManagement
+
+    Use:                Gets a total of recipients for each custom accepted domain
+                        Exports to CSV by default
+
+    Updates:        
+#>
 
 
 
