cb7beb44ce0114f6a9f2e73c06dcf3bd95803f50
Great-Yarmouth-Space-Program <141946788+Great-Yarmouth-Space-Program@users.noreply.github.com>
2023-08-23 09:19:08 +0100

    Update

---
+++ GYSP-NewSharedMailbox.ps1
@@ -0,0 +1,31 @@
+﻿<#DISCLAIMER:
+
+    By running this PowerShell script, you acknowledge and accept that:
+    
+    - You execute this script at your own risk and discretion.
+    - The author of this script is not responsible for any damage, data loss, or unintended consequences 
+      that may result from executing this script.
+    - It is recommended to test this script in a controlled environment before use in a production scenario.
+
+    If you do not agree with these terms, please refrain from executing this script.
+
+
+
+
+<#
+
+Export Mailboxeds from source
+
+$mailboxes = Get-mailbox -ResultSize unlimited
+$sharedmailbox = $mailboxes | Where{$_.RecipientTypeDetails -eq "SharedMailbox"}
+$sharedmailbox | Select name,alias,Primary* | Export-Csv SharedMailboxes.csv -NoTypeInformation
+
+#>
+
+$NewSharedmailboxes = Import-Csv SharedMailboxes.csv
+
+Foreach($NewSharedmailbox in $NewSharedmailboxes) {
+
+New-Mailbox -Shared -Name $NewSharedmailbox.Name -DisplayName $NewSharedmailbox.Name -Alias $NewSharedmailbox.Alias 
+
+}
